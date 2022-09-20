8c8a4d73031b756e36bade3477381f81896ff491
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-20 15:10:57 +0530

    added label and applied css

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -3,9 +3,11 @@ import "./MultiSelect.css"
 export default function MultiSelect() {
   return (
     <div className='multi_select'>
+      <label htmlFor="">Select Values</label> <br />
        <select
-       placeholder='select values'
+          placeholder='Select Options'
     >
+      <option value=""></option>
         <option value="value1">option1</option>
         <option value="value2">option2</option>
         <option value="value3">option3</option>
