c5d85bc4c51c18f737a1e7926eaebd0abbfc0031
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-20 15:48:00 +0530

    added custom option data

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./MultiSelect.css"
 export default function MultiSelect() {
+  let optionData=[
+    {
+      option:"Option 1",
+      value:"value 1"
+    },
+    {
+      option:"Option 2",
+      value:"value 2"
+    },
+    {
+      option:"Option 3",
+      value:"value 3"
+    }
+  ]
+  const [selected,setSelected]=useState([])
+  const multiSelectHanlder=(e)=>{
+     const {value}= e.target;
+      if(selected.includes(value)){
+        setSelected(selected.filter(e=>e!==value))
+      }
+      else{
+         let selectedValues= selected;
+         selectedValues.push(value);
+         setSelected(selectedValues)
+      }
+  }
+  console.log(selected);
   return (
     <div className='multi_select'>
       <label htmlFor="">Select Values</label> <br />
        <select
           placeholder='Select Options'
+          onChange={multiSelectHanlder}
+          value={selected.join(",")}
     >
       <option value=""></option>
-        <option value="value1">option1</option>
-        <option value="value2">option2</option>
-        <option value="value3">option3</option>
-        <option value="value4">option4</option>
+       {
+        optionData.map((op)=>{
+          return  <option value={op.value}> {op.option}</option>
+        })
+       }
       </select>
     </div>
   )
